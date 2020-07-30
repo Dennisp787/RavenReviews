c3878ada6b98f5f4058a4ef2e502957688fa4713
DennisP <37181511+Dennisp787@users.noreply.github.com>
2020-07-30 13:55:44 -0400

    Add files via upload

---
+++ RavenReviews/mr1.js
@@ -0,0 +1,8 @@
+var drop = document.querySelector('.drop');
+var hidden = document.querySelector('.hidden');
+
+
+drop.addEventListener("click", function(){
+  hidden.classList.toggle('hidden');
+
+})
