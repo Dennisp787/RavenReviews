206f2e2642ad0d868305d9b30fabf9a679db5e82
DennisP <37181511+Dennisp787@users.noreply.github.com>
2021-01-15 22:38:52 -0500

    Delete RavenReviews directory

--- RavenReviews/mr1.js
+++
@@ -1,8 +0,0 @@
-var drop = document.querySelector('.drop');
-var hidden = document.querySelector('.hidden');
-
-
-drop.addEventListener("click", function(){
-  hidden.classList.toggle('hidden');
-
-})
